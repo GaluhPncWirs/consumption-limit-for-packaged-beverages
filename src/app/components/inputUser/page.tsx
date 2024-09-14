a761c404fbf70a4e21b69ab02caa05a9bd404764
galuh panca wirasa <Galuh panca wirasa>
2024-09-14 14:57:35 +0700

    initial commit

---
+++ src/app/components/inputUser/page.tsx
@@ -0,0 +1,272 @@
+"use client";
+import { useRef, useState } from "react";
+
+export default function DisplayInputUser() {
+  // function calculateCalories() {
+  //     const age = parseInt(document.getElementById("age").value);
+  //     const gender = document.querySelector("input[name=option]:checked").value;
+  //     const weight = parseFloat(document.getElementById("weight").value);
+  //     const height = parseFloat(document.getElementById("height").value);
+  //     const activityLevel = document.getElementById("activityLevel").value;
+  //     const sugarContent = parseFloat(
+  //       document.getElementById("sugarContent").value
+  //     );
+  //     const volumeKemasan = parseFloat(
+  //       document.getElementById("volumeKemasan").value
+  //     );
+  //     const healthStatus = document.getElementById("healthStatus").value;
+  //     const result = document.getElementById("result");
+  //     const perBottle = document.getElementById("perBotol");
+  //     let BMR; // Basal Metabolic Rate
+
+  //     // Hitung BMR berdasarkan jenis kelamin
+  //     if (gender === "male") {
+  //       BMR = 10 * weight + 6.25 * height - 5 * age + 5;
+  //     }
+
+  //     if (gender === "female") {
+  //       BMR = 10 * weight + 6.25 * height - 5 * age - 161;
+  //     }
+
+  //     // Tentukan faktor aktivitas
+  //     let activityFactor;
+  //     switch (activityLevel) {
+  //       case "sedentary":
+  //         activityFactor = 1.2;
+  //         break;
+  //       case "lightlyActive":
+  //         activityFactor = 1.375;
+  //         break;
+  //       case "moderatelyActive":
+  //         activityFactor = 1.55;
+  //         break;
+  //       case "veryActive":
+  //         activityFactor = 1.725;
+  //         break;
+  //       case "extraActive":
+  //         activityFactor = 1.9;
+  //         break;
+  //       default:
+  //         activityFactor = 1.2;
+  //     }
+
+  //     const TDEE = BMR * activityFactor;
+  //     let maxSugarCalories;
+
+  //     // Sesuaikan berdasarkan status kesehatan
+  //     if (healthStatus === "overweight") {
+  //       maxSugarCalories = TDEE * 0.08;
+  //     } else if (healthStatus === "diabetic") {
+  //       maxSugarCalories = TDEE * 0.05;
+  //     } else {
+  //       maxSugarCalories = TDEE * 0.1;
+  //     }
+
+  //     const maxSugarGrams = maxSugarCalories / 4; // Karena 1 gram gula = 4 kalori
+  //     const product = sugarContent / volumeKemasan;
+  //     const maxConsumptionMl = maxSugarGrams / product;
+
+  //     // console.log("Kebutuhan Kalori Harian (TDEE):" + TDEE.toFixed(2));
+  //     result.textContent = `Konsumsi Gula Maksimal (gram): ${maxSugarGrams.toFixed(
+  //       2
+  //     )}`;
+
+  //     if (maxConsumptionMl >= volumeKemasan) {
+  //       perBottle.textContent = `Konsumsi per: ${Math.floor(
+  //         maxConsumptionMl / volumeKemasan
+  //       )} botol`;
+  //     } else {
+  //       perBottle.textContent = `Minuman ini bisa anda konsumsi :  ${maxConsumptionMl.toFixed(
+  //         2
+  //       )} ml, kurang dari satu botol.`;
+  //     }
+  //   }
+
+  const male = useRef(null);
+  const female = useRef(null);
+  const ages = useRef(null);
+  const bodyHeight = useRef(null);
+  const bodyWeight = useRef(null);
+  const activityLevel = useRef(null);
+  const healthStatus = useRef(null);
+  const [yourCalories, setYourCalories] = useState("");
+
+  function calculateCalories() {
+    const age = parseInt(ages.current?.value);
+    const weight = parseInt(bodyWeight.current?.value);
+    const height = parseFloat(bodyHeight.current?.value);
+    let BMR; // Basal Metabolic Rate
+
+    if (male.current?.checked) {
+      if (male.current?.value === "male") {
+        BMR = 10 * weight + 6.25 * height - 5 * age + 5;
+      }
+    } else {
+      if (female.current?.value === "female") {
+        BMR = 10 * weight + 6.25 * height - 5 * age - 161;
+      }
+    }
+
+    let activityFactor;
+    switch (activityLevel.current?.value) {
+      case "sedentary":
+        activityFactor = 1.2;
+        break;
+      case "lightlyActive":
+        activityFactor = 1.375;
+        break;
+      case "moderatelyActive":
+        activityFactor = 1.55;
+        break;
+      case "veryActive":
+        activityFactor = 1.725;
+        break;
+      case "extraActive":
+        activityFactor = 1.9;
+        break;
+      default:
+        activityFactor = 1.2;
+    }
+
+    const TDEE = BMR! * activityFactor;
+    setYourCalories(TDEE.toFixed(2));
+    // let maxSugarCalories;
+
+    // // Sesuaikan berdasarkan status kesehatan
+    // if (healthStatus.current?.value === "overweight") {
+    //   maxSugarCalories = TDEE * 0.08;
+    // } else if (healthStatus.current?.value === "diabetic") {
+    //   maxSugarCalories = TDEE * 0.05;
+    // } else {
+    //   maxSugarCalories = TDEE * 0.1;
+    // }
+
+    console.log("Kebutuhan Kalori Harian (TDEE):" + TDEE.toFixed(2));
+  }
+
+  return (
+    <>
+      <div className="w-full">
+        <div>
+          {yourCalories}
+          <h1 className="text-center mb-5 text-xl font-bold">
+            Penghitung Konsumsi Batas Aman Konsumsi Gula Harian{" "}
+          </h1>
+
+          <div className="bg-blue-300 rounded-lg py-5 px-3 max-w-xl mx-auto shadow-lg">
+            <div className="mb-5 text-center text-lg font-semibold">
+              <h2>
+                silahkan inputkan di bawah ini agar bisa mengetahui berapa
+                kalori anda{" "}
+              </h2>
+            </div>
+            <form
+              id="sugarForm"
+              className="flex flex-col justify-center gap-4 ml-5"
+            >
+              <div id="inputGender" className="flex gap-2 items-center">
+                <span className="text-lg">Gender : </span>
+                <input type="radio" value="male" name="option" ref={male} />
+                <label htmlFor="gender">Pria</label>
+                <input type="radio" value="female" name="option" ref={female} />
+                <label htmlFor="gender">Wanita</label>
+              </div>
+
+              <div className="relative w-1/2 py-3">
+                <input
+                  type="number"
+                  id="age"
+                  className="inputField peer"
+                  ref={ages}
+                />
+                <label htmlFor="age" className="labelText">
+                  Usia (tahun) :
+                </label>
+              </div>
+
+              <div className="relative w-1/2 py-3">
+                <input
+                  type="number"
+                  id="height"
+                  className="inputField peer"
+                  ref={bodyHeight}
+                />
+                <label htmlFor="height" className="labelText">
+                  Tinggi Badan (cm) :
+                </label>
+              </div>
+
+              <div className="relative w-1/2 pt-3">
+                <input
+                  type="number"
+                  id="weight"
+                  className="inputField peer"
+                  ref={bodyWeight}
+                />
+                <label htmlFor="weight" className="labelText">
+                  Berat Badan (kg) :
+                </label>
+              </div>
+
+              <div>
+                <label htmlFor="activityLevel" className="block mb-2 text-lg">
+                  Activity Level
+                </label>
+                <select
+                  id="activityLevel"
+                  className="cursor-pointer bg-slate-300 rounded-md p-2 text-sm"
+                  ref={activityLevel}
+                >
+                  <option value="sedentary">
+                    Tidak Aktif (Tidak Melakukan Aktifitas Berat)
+                  </option>
+                  <option value="lightlyActive">
+                    Aktif (olahraga ringan 1-3 hari per minggu)
+                  </option>
+                  <option value="moderatelyActive">
+                    Cukup Aktif (olahraga sedang 3-5 hari per minggu)
+                  </option>
+                  <option value="veryActive">
+                    Sangat Aktif (olahraga keras 6-7 hari per minggu)
+                  </option>
+                  <option value="extraActive">
+                    Extra Aktif (olahraga yang sangat keras atau pekerjaan
+                    fisik)
+                  </option>
+                </select>
+              </div>
+
+              <div>
+                <label htmlFor="healthStatus" className="block mb-2 text-lg">
+                  Status Kesehatan
+                </label>
+                <select
+                  id="healthStatus"
+                  className="p-2 rounded-md bg-slate-300 text-sm"
+                  ref={healthStatus}
+                >
+                  <option value="normal">Normal</option>
+                  <option value="overweight">Obesitas</option>
+                  <option value="diabetic">Diabetes</option>
+                </select>
+              </div>
+            </form>
+            <div className="mt-4 mx-auto text-center max-w-20 rounded-md bg-slate-300 hover:bg-slate-400">
+              <button onClick={calculateCalories}>Hitung</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+{
+  /* <label htmlFor="sugarContent">Kadar Gula dalam kemasan Minuman (dalam satuan gram) :</label>
+<input type="number" id="sugarContent" step="0.01" required/>
+
+<label htmlFor="volumeKemasan">volume kemasan (dalam satuan ml) :</label>
+<input type="number" id="volumeKemasan" step="0.01" required/>
+
+<button type="button" >Hitung Konsumsi Maksimal</button> */
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,78 @@
-import Image from "next/image";
+import DisplayInputUser from "./components/inputUser/page";
 
 export default function Home() {
+  const clientID = "c94e3677e63145e1a05fe557f002e60b";
+  const clientSecret = "4570aa7010cb46ccacec36c900e7a64f";
+  async function getData() {
+    const url = "https://oauth.fatsecret.com/connect/token";
+
+    const header = new Headers({
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + btoa(clientID + ":" + clientSecret),
+    });
+
+    const body = new URLSearchParams({
+      grant_type: "client_credentials",
+      scope: "basic",
+    });
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: header,
+        body: body,
+      });
+
+      if (!response.ok) {
+        throw new Error("http error status" + response.status);
+      }
+      const data = await response.json();
+
+      await callApi(data.access_token, "toast");
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
+
+  async function callApi(accessToken: string, searchExpression: string) {
+    const apiUrl = "https://platform.fatsecret.com/rest/server.api";
+
+    const headers = new Headers({
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    });
+
+    const params = new URLSearchParams({
+      method: "foods.search",
+      search_expression: searchExpression,
+      format: "json",
+    });
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: headers,
+        body: params.toString(),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("Food Search Response:", data);
+    } catch (error) {
+      console.error("Error calling API:", error);
+    }
+  }
+  // getData();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className="bg-orange-300 ">
+        <div className="h-screen max-w-2xl mx-auto flex flex-col items-center justify-center">
+          <DisplayInputUser />
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
