e1aac8c17ee1640aa8a16c565154e68f174a63c8
galuh panca wirasa <Galuh panca wirasa>
2024-09-22 20:38:39 +0700

    perubahan minor

--- src/app/mainContent/page.tsx
+++ src/app/mainContent/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useCalories } from "@/context/useContex";
+import Image from "next/image";
 import { useRef } from "react";
 
 export default function MainContent() {
@@ -31,9 +32,9 @@ export default function MainContent() {
   }
   return (
     <div className="bg-orange-300">
-      <div className="w-5/6 bg-blue-300 mx-auto h-screen">
-        <div className="w-11/12 mx-auto">
-          <div className="pt-5 pl-10">
+      <div className="w-5/6 mx-auto">
+        <div className="w-11/12 mx-auto bg-blue-300 h-full flex flex-col justify-center">
+          <div className="mt-10 flex justify-around">
             <p className="mr-2">
               Your Max Consume Sugar Per Day :{" "}
               <span>
@@ -42,10 +43,16 @@ export default function MainContent() {
                 })}
               </span>
             </p>
+            <div className="bg-red-400 h-48 w-60">
+              <p>LeaderBoard</p>
+            </div>
           </div>
-          <div className="mt-7">
-            <form action="" className="flex justify-evenly">
-              <div className="relative w-1/3 py-3">
+          <div className="mt-7 flex items-center">
+            <form
+              action=""
+              className="basis-4/6 flex flex-col items-center justify-center"
+            >
+              <div className="relative w-1/2 py-3">
                 <input
                   type="number"
                   id="sugarContent"
@@ -58,7 +65,7 @@ export default function MainContent() {
                   Kadar Gula dalam Minuman (gram) :
                 </label>
               </div>
-              <div className="relative w-1/3 py-3">
+              <div className="relative w-1/2 py-3">
                 <input
                   type="number"
                   id="volumeKemasan"
@@ -72,14 +79,23 @@ export default function MainContent() {
                 </label>
               </div>
             </form>
-            <button
-              type="button"
-              className="bg-blue-500 px-3 py-1 rounded-lg hover:bg-blue-400"
-              onClick={calculateMaximal}
-            >
-              Hitung
-            </button>
+            <div className="basis-2/6">
+              <Image
+                src="/images/plastic-bottle-water.png"
+                alt="bottle"
+                width={300}
+                height={400}
+                className="bg-slate-100"
+              />
+            </div>
           </div>
+          <button
+            type="button"
+            className="bg-blue-500 px-3 py-1 rounded-lg hover:bg-blue-400"
+            onClick={calculateMaximal}
+          >
+            Hitung
+          </button>
         </div>
       </div>
     </div>
